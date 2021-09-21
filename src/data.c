4d2e1ae7aab6ee6efcf138dfaabe0221cb84a3ca
Janeka <joaoaganjos@gmail.com>
2021-09-21 18:17:25 +0100

    Update data.c

--- src/data.c
+++ src/data.c
@@ -18,8 +18,7 @@ struct data_t *data_create(int size){
 struct data_t *data_create2(int size, void *data){
     struct data_t* const temp = malloc(sizeof(struct data_t));
     temp->datasize = size;
-    temp->data = malloc(size);
-    memcpy(temp->data, data, size);
+    temp->data = data; // TODO: copia-se o buffer ou apodera-se dele
     return temp;
 }
 
@@ -48,6 +47,5 @@ struct data_t *data_dup(struct data_t *data){
 void data_replace(struct data_t *data, int new_size, void *new_data){
     free(data->data);
     data->datasize = new_size;
-    data->data = malloc(new_size);
-    memcpy(data->data, new_data, new_size);
+    data->data = new_data;
 }
