0a7506dba9332a92bced052e96224d49ca568197
Denomycor <afonsoresteves@hotmail.com>
2021-10-19 22:17:36 +0100

    started table_kel.c

--- source/network_server.c
+++ source/network_server.c
@@ -1,11 +1,32 @@
 
 #include "table_skel.h"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 /* Função para preparar uma socket de receção de pedidos de ligação
  * num determinado porto.
  * Retornar descritor do socket (OK) ou -1 (erro).
  */
-int network_server_init(short port);
+int network_server_init(short port){
+    int sockfd;
+    struct sockaddr_in server;
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+        return -1;
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(sockfd, (struct sockaddr *) &server, sizeof(server)) < 0){
+        close(sockfd);
+        return -1;
+    };
+
+    return sockfd;
+}
 
 /* Esta função deve:
  * - Aceitar uma conexão de um cliente;
@@ -14,7 +35,19 @@ int network_server_init(short port);
  * - Esperar a resposta do skeleton;
  * - Enviar a resposta ao cliente usando a função network_send.
  */
-int network_main_loop(int listening_socket);
+int network_main_loop(int listening_socket){
+    
+    
+    if (listen(listening_socket, 0) < 0){
+        return -1;
+    };
+
+    //Server waiting for connections
+
+    
+
+
+}
 
 /* Esta função deve:
  * - Ler os bytes da rede, a partir do client_socket indicado;
--- source/table_skel.c
+++ source/table_skel.c
@@ -1,6 +1,11 @@
 
 #include "sdmessage.pb-c.h"
 #include "table.h"
+#include "entry.h"
+#include "serialization.h"
+#include "string.h"
+
+extern struct table_t g_table;
 
 /* Inicia o skeleton da tabela.
  * O main() do servidor deve chamar esta função antes de poder usar a
@@ -8,7 +13,10 @@
  * serem usadas pela tabela mantida no servidor.
  * Retorna 0 (OK) ou -1 (erro, por exemplo OUT OF MEMORY)
  */
-int table_skel_init(int n_lists);
+int table_skel_init(int n_lists){
+
+    
+}
 
 /* Liberta toda a memória e recursos alocados pela função table_skel_init.
  */
@@ -18,5 +26,92 @@ void table_skel_destroy();
  * e utiliza a mesma estrutura MessageT para devolver o resultado.
  * Retorna 0 (OK) ou -1 (erro, por exemplo, tabela nao incializada)
 */
-int invoke(MessageT *msg);
+int invoke(MessageT *msg){
+
+
+    if(msg->opcode == MESSAGE_T__OPCODE__OP_SIZE){
+        msg->opcode++;
+        msg->c_type == MESSAGE_T__C_TYPE__CT_RESULT;
+        msg->data_size = 4;
+        msg->data = malloc(sizeof(int));
+        *msg->data = table_size(&g_table);
+
+    }else if(msg->opcode == MESSAGE_T__OPCODE__OP_DEL){
+        if(table_del(&g_table, msg->data)==-1){
+            msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+        }else{
+            msg->opcode++;
+        }
+        msg->c_type == MESSAGE_T__C_TYPE__CT_NONE;
+        free(msg->data);
+        msg->data_size = 0;
+        msg->data = NULL;
+
+    }else if(msg->opcode == MESSAGE_T__OPCODE__OP_GET){
+        struct data_t* temp;
+        if(temp = table_get(&g_table, msg->data) == NULL){
+            msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+            msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+            free(msg->data);
+            msg->data = NULL;
+            msg->data_size = 0;
+
+        }else{
+            msg->opcode++;
+            msg->c_type = MESSAGE_T__C_TYPE__CT_VALUE;
+            free(msg->data);
+            msg->data = temp;
+            msg->data_size = sizeof(struct data_t);
+        }
+
+    }else if(msg->opcode == MESSAGE_T__OPCODE__OP_PUT){
+        struct entry_t* temp = buffer_to_entry(msg->data, msg->data_size);
+        if(table_put(&g_table, temp->key, temp->value)==-1){
+            msg->opcode = MESSAGE_T__OPCODE__OP_ERROR;
+        }else{
+            msg->opcode++;
+        }
+        msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
+        free(temp);
+        free(msg->data);
+        msg->data = NULL;
+        msg->data_size = 0;
+
+    }else if(msg->opcode == MESSAGE_T__OPCODE__OP_GETKEYS){
+        msg->opcode++;
+        msg->c_type = MESSAGE_T__C_TYPE__CT_KEYS;
+        msg->data_size=0;
+
+        char** k = table_get_keys(&g_table);
+        int r=0;
+        int buf_size=0;
+
+        while(k[r]!=NULL){  
+            msg->data_size++; //count nr of strings
+            buf_size += strlen(k[r++])+1; //acc for chars in single buff size
+        }
+
+        buf_size = buf_size*sizeof(char); //multiply for sizeof chars
+        char* buf = malloc(buf_size); //aloc single buf
+
+        r=0;
+        char* l = buf;
+        while(k[r]!=NULL){
+            memcpy(l, k[r++], sizeof(char)*(strlen(l)+1)); ///copy for single buff
+            l += sizeof(char)*(strlen(l)+2); //advance ptr to the start of next string
+        }
+
+        msg->data = buf;
+
+        table_free_keys(k);
+
+    }else if(msg->opcode == MESSAGE_T__OPCODE__OP_PRINT){
+
+
+
+    }else{
+        return -1;
+    }
+    return 0;
+}
 
