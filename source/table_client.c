34e79dd0aec7e01ede7c6596dcb916dda1fe6c25
Vicente Sousa <fc55386@alunos.fc.ul.pt>
2021-10-20 18:23:41 +0100

    server main drafted

--- source/table_client.c
+++ source/table_client.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]) {
 
     if (argc != 2) {
         printf("Deve chamar apenas com um argumento no seguinte formato -> [address]:[port]\n");
+        exit(0);
     }
 
     struct rtable_t *tab = rtable_connect(argv[1]);
