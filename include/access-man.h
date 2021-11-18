5293dba4f8f092b1ff6a5b10d7a4112a9308fe98
Denomycor <afonsoresteves@hotmail.com>
2021-11-18 23:20:26 +0000

    Added Documentation to access-man

--- include/access-man.h
+++ include/access-man.h
@@ -4,23 +4,44 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+
+/* Struct thats defines a mutex thats never allows running read and
+ * write operations at the same time, however, they can run at the same
+ * time if they are both read or write
+ */
 typedef struct {
     size_t rc, wc;
     pthread_mutex_t mutex;
 } rw_exc_t;
 
+/* Initialize the passed memory, fowards pthread_mutex_init return value
+ */
 int rw_exc_init(rw_exc_t* m);
 
+/* Locks a mutex for a function that is responsible for reading values,
+ * fowards pthread_mutex_lock return value
+ */
 int read_exclusive_lock(rw_exc_t* m);
 int read_exc(rw_exc_t* m);
 
+/* Unlocks a mutex for a function that is responsible for reading values,
+ * fowards pthread_mutex_unlock return value
+ */
 int read_exclusive_unlock(rw_exc_t* m);
 
+/* Locks a mutex for a function that is responsible for writing values,
+ * fowards pthread_mutex_lock return value
+ */
 int write_exclusive_lock(rw_exc_t* m);
 int write_exc(rw_exc_t* m);
 
+/* Unlocks a mutex for a function that is responsible for writing values,
+ * fowards pthread_mutex_unlock return value
+ */
 int write_exclusive_unlock(rw_exc_t* m);
 
+/* Destroys the passed struct, fowards pthread_mutex_destroy return value
+ */
 int rw_exc_destroy(rw_exc_t* m);
 
 #endif
