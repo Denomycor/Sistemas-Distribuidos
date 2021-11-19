93292ad3029434ef220bccbea515d4694d8b7d5a
Vicente Sousa <fc55386@alunos.fc.ul.pt>
2021-11-19 12:35:34 +0000

    #20 Removed unused functions

--- include/helper/priv-func.h
+++ include/helper/priv-func.h
@@ -7,12 +7,6 @@
 #ifndef _PRIV_FUNC_H
 #define _PRIV_FUNC_H
 
-//Gets the number of chars in a string, including the \0 char (==strlen()+1)
-int strchars(const char* str);
-
-//Gets the number of bytes of a string (==(strlen()+1)*sizeof(char))
-int strmem(const char* str);
-
 //appends a string to another
 void strapp(char** str, const char* app);
 
